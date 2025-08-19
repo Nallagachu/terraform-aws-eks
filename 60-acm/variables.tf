d7a2daa6134c6c34787187067e8c0da099d6a4f2
durgababu <durgababu.nallagachu@gmail.com>
2025-08-19 12:51:58 +0530

    testing-version-upgrade

---
+++ 60-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z10031831EHO6D6LW9W2V"
+}
+
+variable "zone_name" {
+    default = "saijyo.store"
+}
