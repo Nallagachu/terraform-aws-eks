51509afb8587fcfd13acd464252ee14e0cea9cdf
durgababu <durgababu.nallagachu@gmail.com>
2025-08-25 04:50:58 +0530

    1.32

--- 80-eks/main.tf
+++ 80-eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 21.0" # this is module version
 
   name               = "${var.project}-${var.environment}"
-  kubernetes_version = "1.32"
+  kubernetes_version = "1.33"
 
   addons = {
     coredns                = {}
